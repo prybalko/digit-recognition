39b08d3c7e3582208e84ced535046168004f723b
Pavel Rybalko <pavel.rybalko@quantumsoft.ru>
2018-04-26 12:19:22 +0300

    first model

--- main.py
+++ main.py
@@ -1,9 +1,93 @@
+import math
+
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
 
 dataset = pd.read_csv("data/train-less.csv", sep=",")
 
-for i in range(9):
-    plt.subplot(3, 3, i+1)
-    plt.imshow(dataset.iloc[i][1:].values.reshape([28, 28]))
-plt.show()
+# for i in range(9):
+#     plt.subplot(3, 3, i+1)
+#     plt.imshow(dataset.iloc[i][1:].values.reshape([28, 28]))
+# plt.show()
+# print dataset
+
+feature_columns = [tf.feature_column.numeric_column("pixel0")]
+my_feature = dataset[['pixel0']]
+
+targets = dataset['label']
+
+# Use gradient descent as the optimizer for training the model.
+my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.0000001 for Gradient Descent.
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
+
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of one feature.
+
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+
+    # Convert pandas data into a dict of np arrays.
+    features = {key: np.array(value) for key, value in dict(features).items()}
+
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features, targets))  # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    # Shuffle the data, if specified.
+    if shuffle:
+        ds = ds.shuffle(buffer_size=10000)
+
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+
+_ = linear_regressor.train(
+    input_fn=lambda: my_input_fn(my_feature, targets),
+    steps=100
+)
+
+# Create an input function for predictions.
+# Note: Since we're making just one prediction for each example, we don't
+# need to repeat or shuffle the data here.
+prediction_input_fn =lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
+
+# Call predict() on the linear_regressor to make predictions.
+predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+
+# Format predictions as a NumPy array, so we can calculate error metrics.
+predictions = np.array([item['predictions'][0] for item in predictions])
+
+# Print Mean Squared Error and Root Mean Squared Error.
+mean_squared_error = metrics.mean_squared_error(predictions, targets)
+root_mean_squared_error = math.sqrt(mean_squared_error)
+print "Mean Squared Error (on training data): %0.3f" % mean_squared_error
+print "Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error
+
+
+calibration_data = pd.DataFrame()
+calibration_data["predictions"] = pd.Series(predictions)
+calibration_data["targets"] = pd.Series(targets)
+print calibration_data.describe()
