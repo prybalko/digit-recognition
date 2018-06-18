a18fe1d60192acfce8b10f0d5bace017f33f56b6
Pavel Rybalko <pavel.rybalko@quantumsoft.ru>
2018-06-18 13:43:56 +0300

    migrate to scikit

--- main.py
+++ main.py
@@ -1,89 +1,38 @@
-""" K-Means.
-
-Implement K-Means algorithm with TensorFlow, and apply it to classify
-handwritten digit images. This example is using the MNIST database of
-handwritten digits as training samples (http://yann.lecun.com/exdb/mnist/).
-
-Note: This example requires TensorFlow v1.1.0 or over.
-
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-"""
-
-from __future__ import print_function
-
-import numpy as np
-import tensorflow as tf
-from tensorflow.contrib.factorization import KMeans
-from matplotlib import cm
-from matplotlib import gridspec
-from matplotlib import pyplot as plt
-
-# Ignore all GPUs, tf random forest does not benefit from it.
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("data/", one_hot=True)
-full_data_x = mnist.train.images
+from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils import shuffle
 
-# Parameters
-num_steps = 50  # Total steps to train
-batch_size = 1024  # The number of samples per batch
-k = 25  # The number of clusters
-num_classes = 10  # The 10 digits
-num_features = 784  # Each image is 28x28 pixels
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# Input images
-X = tf.placeholder(tf.float32, shape=[None, num_features])
-# Labels (for assigning a label to a centroid and testing)
-Y = tf.placeholder(tf.float32, shape=[None, num_classes])
+mnist = datasets.fetch_mldata('MNIST original', data_home=dir_path)
+mnist.data, mnist.target = shuffle(mnist.data, mnist.target)
 
-# K-Means Parameters
-kmeans = KMeans(inputs=X, num_clusters=k, distance_metric='cosine', use_mini_batch=True)
+images_and_labels = list(zip(mnist.data, mnist.target))
 
-# Build KMeans graph
-training_graph = kmeans.training_graph()
+# To apply a classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = mnist.data.shape[0]
+data = mnist.data
 
-(all_scores, cluster_idx, scores, cluster_centers_initialized, cluster_centers_var, init_op, train_op) = training_graph
+# Create a classifier: a KNN classifier
+classifier = KNeighborsClassifier()
 
-cluster_idx = cluster_idx[0] # fix for cluster_idx being a tuple
-avg_distance = tf.reduce_mean(scores)
+# We learn the digits on the first half of the digits
+classifier.fit(data[:n_samples // 2], mnist.target[:n_samples // 2])
 
-# Initialize the variables (i.e. assign their default value)
-init_vars = tf.global_variables_initializer()
+# Now predict the value of the digit on the second half:
+expected = mnist.target[-10:]
+predicted = classifier.predict(data[-10:])
 
-# Start TensorFlow session
-sess = tf.Session()
+images_and_predictions = list(zip(mnist.data[-10:], predicted))
+for index, (image, prediction) in enumerate(images_and_predictions[:-1]):
+    plt.subplot(3, 3, index+1)
+    plt.axis('off')
+    image = image.reshape((28, 28))
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title('Prediction: %i' % prediction)
 
-# Run the initializer
-sess.run(init_vars, feed_dict={X: full_data_x})
-sess.run(init_op, feed_dict={X: full_data_x})
-
-# Training
-for i in range(1, num_steps + 1):
-    _, d, idx = sess.run([train_op, avg_distance, cluster_idx], feed_dict={X: full_data_x})
-    if i % 10 == 0 or i == 1:
-        print("Step %i, Avg Distance: %f" % (i, d))
-
-# Assign a label to each centroid
-# Count total number of labels per centroid, using the label of each training
-# sample to their closest centroid (given by 'idx')
-counts = np.zeros(shape=(k, num_classes))
-for i in range(len(idx)):
-    counts[idx[i]] += mnist.train.labels[i]
-# Assign the most frequent label to the centroid
-labels_map = [np.argmax(c) for c in counts]
-labels_map = tf.convert_to_tensor(labels_map)
-
-# Evaluation ops
-# Lookup: centroid_id -> label
-cluster_label = tf.nn.embedding_lookup(labels_map, cluster_idx)
-# Compute accuracy
-correct_prediction = tf.equal(cluster_label, tf.cast(tf.argmax(Y, 1), tf.int32))
-accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-# Test Model
-test_x, test_y = mnist.test.images, mnist.test.labels
-print("Test Accuracy:", sess.run(accuracy_op, feed_dict={X: test_x, Y: test_y}))
+plt.show()
