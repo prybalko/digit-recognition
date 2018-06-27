7931406d45c8d6c7591cc1047312093c699d147b
Pavel Rybalko <pavel.rybalko@quantumsoft.ru>
2018-06-27 11:50:12 +0300

    crop the input image

--- classifier.py
+++ classifier.py
@@ -1,14 +1,15 @@
 import os
 
+from skimage.transform import resize
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
 from skimage.morphology import closing, square
+import numpy as np
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -18,7 +19,7 @@ class Classifier(object):
     def __init__(self):
         self.mnist = datasets.fetch_mldata('MNIST original', data_home=dir_path)
         self.classifier = KNeighborsClassifier()
-        self.classifier.fit(self.mnist.data[::5], self.mnist.target[::5])
+        self.classifier.fit(self.mnist.data, self.mnist.target)
 
     def predict(self, image):
         thresh = threshold_otsu(image)
@@ -31,35 +32,24 @@ class Classifier(object):
         # image_label_overlay = label2rgb(label_image, image=image)
 
         fig, ax = plt.subplots(figsize=(8, 6))
-        ax.imshow(cleared)
 
         for region in regionprops(label_image):
             print region.bbox, region.area, region.centroid
             # take regions with large enough areas
             if region.area >= 10:
                 # draw rectangle around segmented coins
-                oy, ox, maxr, maxc = region.bbox
+                oy, ox, maxr, maxc = (int(x) for x in region.bbox)
                 width, height = maxc - ox, maxr - oy
-                rect = mpatches.Rectangle((ox, oy), width, height,
-                                          fill=False, edgecolor='green', linewidth=2)
-                ax.add_patch(rect)
-                cy, cx = region.centroid
-
-                half_side = max(cx-ox, width-(cx-ox), cy-oy, height-(cy-oy))
-                big_rect = mpatches.Rectangle((cx-half_side, cy-half_side), half_side*2, half_side*2,
-                                              fill=False, edgecolor='red', linewidth=2)
-                ax.add_patch(big_rect)
-                ax.scatter(cx, cy, s=400, c='C0', marker='+', linewidth=2)
-
-        ax.set_axis_off()
-        plt.tight_layout()
-        plt.show()
-
-
-
-        # resized_img = resize(image, (28, 28), anti_aliasing=True)
-        # resized_img *= 255. / resized_img.max()
-        # return self.classifier.predict(image.reshape((1, 784)))[0]
-
+                cy, cx = (int(x) for x in region.centroid)
+                half_side = int(max(cx-ox, width-(cx-ox), cy-oy, height-(cy-oy)))
+                size_size = half_side*2
+                x = cx-half_side
+                y = cy-half_side
+                cropped = image[y:y+size_size, x:x+size_size]
+                resized_img = resize(cropped, (20, 20), anti_aliasing=True)
+                resized_img *= 255. / resized_img.max()
+                result = np.zeros((28, 28))
+                result[4:-4, 4:-4] = resized_img
+                return self.classifier.predict(result.reshape((1, 784)))[0]
 
 classifier = Classifier()
--- run.py
+++ run.py
@@ -4,8 +4,6 @@ import io
 import cherrypy
 import numpy as np
 from PIL import Image
-from matplotlib import pyplot as plt
-from skimage.transform import resize
 
 from classifier import classifier
 
@@ -23,11 +21,8 @@ class HelloWorld(object):
         img_data = cherrypy.request.body.read().split('base64,')[1]
         bin_image = Image.open(io.BytesIO(base64.b64decode(img_data))).convert('L')
         original_img = np.asarray(bin_image)
-
-        # plt.imshow(resized_img)
-        # plt.show()
         prediction = classifier.predict(original_img)
-        return {'status': 'ok', 'prediction': int(0)}
+        return {'status': 'ok', 'prediction': int(prediction)}
 
 
 if __name__ == '__main__':
