ea7328845cebbeffbeaa72cbc5c919ce04772535
Pavel Rybalko <pavel.rybalko@quantumsoft.ru>
2018-07-12 11:28:41 +0300

    small changes

--- classifier.py
+++ classifier.py
@@ -1,15 +1,14 @@
 import os
 
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from skimage.segmentation import clear_border
 from skimage.transform import resize
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
-
-from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
-from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
-import numpy as np
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,37 +18,43 @@ class Classifier(object):
     def __init__(self):
         self.mnist = datasets.fetch_mldata('MNIST original', data_home=dir_path)
         self.classifier = KNeighborsClassifier()
-        self.classifier.fit(self.mnist.data, self.mnist.target)
+        self.classifier.fit(self.mnist.data[::5], self.mnist.target[::5])
+
+    def _add_padding(self, img):
+        return np.pad(img, [(4, 4), (4, 4)], mode='constant')
+
+    def _nornalize(self, img):
+        normalized_img = resize(img, (20, 20), anti_aliasing=True)
+        normalized_img *= 255. / img.max()
+        return normalized_img
+
+    def _crop_to_region(self, image, region):
+        oy, ox, maxr, maxc = (int(x) for x in region.bbox)
+        width, height = maxc - ox, maxr - oy
+        cy, cx = (int(x) for x in region.centroid)
+        half_side = int(max(cx - ox, width - (cx - ox), cy - oy, height - (cy - oy)))
+        size_size = half_side * 2
+        x = cx - half_side
+        y = cy - half_side
+        return image[y:y + size_size, x:x + size_size]
+
+    def plot(self, image):
+        plt.imshow(image)
+        plt.show()
 
     def predict(self, image):
         thresh = threshold_otsu(image)
         bw = closing(image > thresh, square(1))
-        # remove artifacts connected to image border
         cleared = clear_border(bw)
-
-        # label image regions
         label_image = label(cleared)
-        # image_label_overlay = label2rgb(label_image, image=image)
-
-        fig, ax = plt.subplots(figsize=(8, 6))
-
+        label_image = self._add_padding(label_image)
         for region in regionprops(label_image):
-            print region.bbox, region.area, region.centroid
-            # take regions with large enough areas
             if region.area >= 10:
-                # draw rectangle around segmented coins
-                oy, ox, maxr, maxc = (int(x) for x in region.bbox)
-                width, height = maxc - ox, maxr - oy
-                cy, cx = (int(x) for x in region.centroid)
-                half_side = int(max(cx-ox, width-(cx-ox), cy-oy, height-(cy-oy)))
-                size_size = half_side*2
-                x = cx-half_side
-                y = cy-half_side
-                cropped = image[y:y+size_size, x:x+size_size]
-                resized_img = resize(cropped, (20, 20), anti_aliasing=True)
-                resized_img *= 255. / resized_img.max()
-                result = np.zeros((28, 28))
-                result[4:-4, 4:-4] = resized_img
-                return self.classifier.predict(result.reshape((1, 784)))[0]
+                cropped = self._crop_to_region(image, region)
+                img = self._nornalize(cropped)
+                img = self._add_padding(img)
+                return self.classifier.predict(img.reshape((1, 784)))[0]
+        return -1
+
 
 classifier = Classifier()
